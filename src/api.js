8d032ed82d53433bad928f8e47575d757e94875b
Timz Mini <timz_mini@Timzs-Mac-mini.local>
2023-04-20 14:03:29 -0700

    updated cors

--- src/api.js
+++ src/api.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 const app = express();
 const router = express.Router();
 
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(morgan('combined'));
+
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: [
+    'http://localhost:3000',
+    'https://clinquant-pavlova-18d509.netlify.app',
+  ],
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
+
+const usermodel = mongoose.model('Users', userSchema);
+
+const ads = [{ title: 'Hello, world (again)!' }];
 
 router.get('/', (req, res) => {
   res.json({
@@ -17,10 +31,8 @@ router.get('/', (req, res) => {
   });
 });
 
-router.get('/test', (req, res) => {
-  res.json({
-    hello: 'test!',
-  });
+router.get('/ads', (req, res) => {
+  res.send(ads);
 });
 
 app.use(`/.netlify/functions/api`, router);
---
+++ src/users/models/users.model.js
@@ -0,0 +1,7 @@
+const userSchema = new Schema({
+  firstName: John,
+  lastName: John,
+  email: John,
+  password: John,
+  permissionLevel: Number,
+});
